67e3eee6daf022d30550b04e0d502e423adba6f1
Nghia19 <trungnghia191919@gmail.com>
2021-03-23 16:03:20 +0700

    update lab2

--- Lab/PS17048-LeTrungNghia_Lab2/Bài4/js/game.js
+++ Lab/PS17048-LeTrungNghia_Lab2/Bài4/js/game.js
@@ -1,10 +1,12 @@
 function game(a) {
-    let x = 10 + Math.round(5 * Math.random())
-    if (a == x) {
-        alert('Chúc mừng bạn đã đoán đúng')
-    } else if (a > x) {
-        alert('Giá trị của bạn lớn hơn số bí mật')
-    } else {
-        alert('Giá trị của bạn nhỏ hơn số bí mật')
-    }
+  let x = 10 + Math.round(5 * Math.random());
+  if (a == x) {
+    document.getElementById("ketqua").innerHTML = "Chúc mừng bạn đã đoán đúng";
+  } else if (a > x) {
+    document.getElementById("ketqua").innerHTML =
+      "Giá trị của bạn lớn hơn số bí mật";
+  } else {
+    document.getElementById("ketqua").innerHTML =
+      "Giá trị của bạn nhỏ hơn số bí mật";
+  }
 }
