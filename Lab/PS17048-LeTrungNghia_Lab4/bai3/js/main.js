4101b5582d5e8166682336924d179a12ee2ea08b
Nghia19 <trungnghia191919@gmail.com>
2021-04-08 15:26:32 +0700

    update

--- Lab/PS17048-LeTrungNghia_Lab4/bai3/js/main.js
+++ Lab/PS17048-LeTrungNghia_Lab4/bai3/js/main.js
@@ -1,23 +1,15 @@
 function clock() {
-  //Khởi tạo đối tượng timer sử dụng Date Object
   var timer = new Date();
-  //Gọi các phương thức của đối tượng timer
-  var hour = timer.getHours(); //Lấy giờ hiện tại (giá trị từ 0 - 23)
-  var minute = timer.getMinutes(); //Lấy phút hiện tại
-  var second = timer.getSeconds(); //Lấy giây  hiện tại
-  //Thêm ký tự 0 đằng trước nếu giờ, phút, giây < 10 với câu lệnh điều khiển if
-  if (hour < 10) {
-    hour = "0" + hour;
-  }
-  if (minute < 10) {
-    minute = "0" + minute;
-  }
-  if (second < 10) {
-    second = "0" + second;
-  }
-  //Hiện thị thời gian lên thẻ div id="clock" với phương thức innerHTML
+  var hour = timer.getHours();
+  var minute = timer.getMinutes();
+  var second = timer.getSeconds();
   document.getElementById("clock").innerHTML =
-    hour + ":" + minute + ":" + second;
+    checkTime(hour) + ":" + checkTime(minute) + ":" + checkTime(second);
 }
-//Thực hiện hàm clock theo chu kỳ 1 giây
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
 setInterval("clock()", 1000);
